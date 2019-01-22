4b726f0b4fd9a0eee048556a858caa254de6858e
tianyang <twu@risd.edu>
2019-01-22 16:28:02 -0500

    adjust animation

--- trend.js
+++ trend.js
@@ -3,6 +3,7 @@ const navclickbar=document.querySelector("ul");
 const navbar=document.querySelector(".n");
 
 console.log(navclickbar.className);
+
 // click on the button to show the list;
 navclickbar.classList.add("hide");
 navbar.classList.remove("tranbackground");
@@ -18,11 +19,27 @@ navclick.onclick=reveal;
 // mouseover the button to show the list;
 navclick.addEventListener("mouseover", function( event ) {
    navclickbar.classList.remove("hide");
+   navclickbar.classList.add("moveleft");
    navbar.classList.add("tranbackground");
+   console.log("working");
+
  })
 
-// mouseout the button to hide the list;
+// mouseover the button to show the list;
+navbar.addEventListener("mouseover", function( event ) {
+
+  console.log("hovering over navbar");
+
+})
+
+// navbar.addEventListener("mouseout", function( event ) {
+//
+//   console.log("left navbar");
+//
+// })
+// mouseout nav to hide the list;
  navbar.addEventListener("mouseout", function( e1 ) {
     navclickbar.classList.add("hide");
     navbar.classList.remove("tranbackground");
+    navclickbar.classList.remove("moveleft");
 })
